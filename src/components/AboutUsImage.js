0676f765f74a6b46840d1b521906a5e446e044ab
ficoCT <fico.dev.ct@gmail.com>
2022-07-06 18:23:19 +0200

    About us page.

--- src/components/AboutUsImage.js
+++ src/components/AboutUsImage.js
@@ -2,7 +2,7 @@ import Image from "../assets/AboutUsImage.jpg";
 
 function AboutUsImage() {
   return (
-      <img src={Image} alt="Skontaktuj się z Nami" className="contact__image" />
+      <img src={Image} alt="Skontaktuj się z Nami" className="aboutUs__image" />
   );
 }
 
